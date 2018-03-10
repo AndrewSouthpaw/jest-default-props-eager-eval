ca7dead77c01f85a46b22e1e1ef78dc2e3ec8919
Andrew Smith <andrewsmith@alumni.stanford.edu>
2018-03-10 12:23:08 +0100

    Use class props.

--- src/module_a/date.jsx
+++ src/module_a/date.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import moment from 'moment'
 
 export class Date extends React.Component {
+  static defaultProps = {
+    foo: console.log('hello I am going to evaluate'),
+    startDate: moment.utc('1950-01-01'),
+  }
+
   render() {
     return (<p>Date!</p>)
   }
-}
-
-Date.defaultProps = {
-  startDate: moment.utc('1950-01-01'),
 }
